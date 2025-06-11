02eef2506347eb48619e09ad2be0b2db5f36555d
moiteiro <bmoiteiro@recordtv.com.br>
2025-06-11 15:07:32 -0300

    configuring rubocop

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,42 +1,101 @@
--- to install new lsp, use :Mason, then select a linter or formmatter that you want
+-- DEBUG
+-- Entrar no :Mason e verificar se há dependencias para serem atualizadas
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"ts_ls",
-					"eslint",
-					'ruby_lsp',
-					"gopls",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    automatic_enable = true,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-			lspconfig.lua_ls.setup({})
-			lspconfig.ts_ls.setup({})
-			lspconfig.eslint.setup({})
-			lspconfig.ruby_lsp.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.gopls.setup({})
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+      local lspconfig = require("lspconfig")
+      lspconfig.tailwindcss.setup({
+        capabilities = capabilities
+      })
+      -- havia configurado com ts_ls mas nao estava reconhecendo meu codigo para sugerir no autocomplete.
+      -- se no futuro parar de funcionar, trocar novamente por ts_ls
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities
+      })
+      -- lspconfig.tsserver.setup({
+      --   capabilities = capabilities
+      -- })
+      lspconfig.solargraph.setup({
+        capabilities = capabilities
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities
+      })
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.gopls.setup({
+        capabilities = capabilities
+      })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
+
+
+-- to install new lsp, use :Mason, then select a linter or formmatter that you want
+-- return {
+-- 	{
+-- 		"williamboman/mason.nvim",
+-- 		config = function()
+-- 			require("mason").setup()
+-- 		end,
+-- 	},
+-- 	{
+-- 		"williamboman/mason-lspconfig.nvim",
+-- 		lazy = false,
+-- 		opts = {
+-- 			auto_install = true,
+-- 		},
+-- 		--config = function()
+-- 		--	require("mason-lspconfig").setup({
+-- 		--		ensure_installed = {
+-- 		--			"lua_ls",
+-- 		--			"ts_ls",
+-- 		--			--"eslint",
+-- 		--			'ruby_lsp',
+-- 		--			"gopls",
+-- 		--		},
+-- 		--	})
+-- 		--end,
+-- 	},
+-- 	{
+-- 		"neovim/nvim-lspconfig",
+-- 		lazy = false,
+-- 		config = function()
+-- 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+-- 			local lspconfig = require("lspconfig")
+-- 
+-- 			lspconfig.lua_ls.setup({})
+-- 			lspconfig.ts_ls.setup({})
+-- 			--lspconfig.eslint.setup({})
+-- 			lspconfig.ruby_lsp.setup({
+-- 				capabilities = capabilities,
+-- 			})
+-- 			lspconfig.gopls.setup({})
+-- 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+-- 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+-- 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+-- 		end,
+-- 	},
+-- }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -2,25 +2,33 @@
 return {
 	"nvimtools/none-ls.nvim",
 	dependencies = {
-    "nvimtools/none-ls-extras.nvim",
+		"nvimtools/none-ls-extras.nvim",
 	},
 	config = function()
 		local null_ls = require("null-ls")
 
 		null_ls.setup({
 			sources = {
-        -- lua
+				-- lua
 				null_ls.builtins.formatting.stylua,
 
-        -- ruby
+				-- ruby
 				-- NOTE: to make rubocop work with docker, is important to configure gems folders
 				-- accessable for both enviroments: it can be done via volume configuration
-				null_ls.builtins.diagnostics.rubocop,
-				null_ls.builtins.formatting.rubocop,
+				null_ls.builtins.diagnostics.rubocop.with({
+					command = "bundle",
+					args = { "exec", "rubocop", "--format", "json", "--force-exclusion", "--stdin", "$FILENAME" },
+					to_stdin = true,
+				}),
+				null_ls.builtins.formatting.rubocop.with({
+					command = "bundle",
+					args = { "exec", "rubocop", "--auto-correct", "--force-exclusion", "--stdin", "$FILENAME", "--stderr" },
+					to_stdin = true,
+				}),
 
-        -- javascript
+				-- javascript
 				null_ls.builtins.formatting.prettier,
-			},
+			}
 		})
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
 	end,
