00991c3779a4e46578d3067ffe2de30f6d360a71
moiteiro <bmoiteiro@recordtv.com.br>
2025-07-17 13:28:46 -0300

    changing to telescope to use fuzzy findding

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,23 +3,67 @@ return {
     "nvim-telescope/telescope-ui-select.nvim",
   },
   {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+    config = function()
+      require("telescope").load_extension("fzf")
+    end,
+  },
+  {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope-fzf-native.nvim",
+    },
     config = function()
       require("telescope").setup({
         extensions = {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown({}),
           },
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case",
+          },
         },
       })
+
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<C-p>", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
       vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 
       require("telescope").load_extension("ui-select")
+      require("telescope").load_extension("fzf")
     end,
   },
 }
+
+-- return {
+--   {
+--     "nvim-telescope/telescope-ui-select.nvim",
+--   },
+--   {
+--     "nvim-telescope/telescope.nvim",
+--     tag = "0.1.5",
+--     dependencies = { "nvim-lua/plenary.nvim" },
+--     config = function()
+--       require("telescope").setup({
+--         extensions = {
+--           ["ui-select"] = {
+--             require("telescope.themes").get_dropdown({}),
+--           },
+--         },
+--       })
+--       local builtin = require("telescope.builtin")
+--       vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+--       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+--       vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+--
+--       require("telescope").load_extension("ui-select")
+--     end,
+--   },
+-- }
